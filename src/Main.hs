d7d89ee5f73a064cb15cdc03b43658dd9bb60cc2
Derek Lam <dc.lam@mail.utoronto.ca>
2020-08-04 00:44:23 -0700

    Allow passing HIE cost centre ID as the function to analyze
    Many changes ended up being necessary to facilitate this:
    
    1. "Locate" the named functions to their binding sites by tying them
    into a GenLocated-ish datatype `Spand` (specialized to RealSrcSpan)
    2. Replace the Bool of `pt_search` with a union type for functions/
    bound names vs. names found in app groups
    3. Repair the cost centre tree code (that tries to "locate" as many
    things in the cost centre tree as possible), amd change its type at the
    top of the traversal from a single tree per HIE to a forest (a possible
    scenario if there the root isn't located but has several descendants that
    are)
    4. Start bounding by the cost centre tree with the following logic:
    
       - Search for bindings/app groups upwards for obvious reasons: the
       symbols obviously depend on their ancestors in the call tree
       - Search the entire subtree below: this is a little less obvious:
       this is to capture the dependencies of functions/names on the values
       they close over
    
    - Repair the regex for the cost centre trees' locations. regex-tdfa is
    POSIX, not Perl. Also, different syntax for one-line srcspans.

--- src/Main.hs
+++ src/Main.hs
@@ -5,15 +5,16 @@ import qualified Data.Set as S
 import qualified Data.Map.Strict as M
 import qualified Data.IntMap as IM
 import qualified Data.Text as T
+import Text.Read ( readMaybe )
 import qualified Data.Text.IO as TIO
 import qualified GHC.Prof as Prof
 import SrcLoc ( SrcLoc(..), SrcSpan, RealSrcSpan(..), mkSrcLoc, mkSrcSpan, containsSpan, mkRealSrcSpan, mkRealSrcLoc, realSrcSpanStart, realSrcSpanEnd )
-import Name ( nameOccName, Name(..) )
-import GHC ( moduleNameString, ModuleName(..) )
+import Name ( nameOccName, nameModule_maybe, Name(..) )
+import GHC ( moduleNameString, moduleName, ModuleName(..) )
 import OccName ( occNameString )
 import Data.Bitraversable ( bisequence )
 import Data.Bifunctor ( bimap )
-import Data.Foldable ( fold )
+import Data.Foldable ( fold, asum )
 import qualified Data.Tree as Tr
 import FastString ( FastString(..), fsLit )
 import NameCache ( NameCache(..), initNameCache )
@@ -25,7 +26,7 @@ import HieUtils
 import Text.Regex.TDFA
 import Text.Regex.Base.RegexLike ( AllTextSubmatches(..) )
 import Data.Monoid ( First(..) )
-import Data.Maybe ( catMaybes, fromMaybe, fromJust, mapMaybe, maybeToList )
+import Data.Maybe ( catMaybes, fromMaybe, fromJust, mapMaybe, maybeToList, listToMaybe )
 import Data.List ( uncons, partition )
 import Data.Foldable ( foldrM, foldlM )
 import Control.Arrow ( (***), (&&&), first, second )
@@ -46,8 +47,6 @@ import Data.Graph.Inductive.PatriciaTree ( Gr(..) )
 
 import System.Directory
 import System.FilePath
-
-import Outputable ( showSDoc, interppSP, Outputable(..) )
 
 import SysTools ( initSysTools )
 import DynFlags ( DynFlags, defaultDynFlags )
@@ -105,11 +104,14 @@ cs_span (Prof.CostCentre {..}) = txt2span =<< costCentreSrc
   where
     txt2span sloc = case sloc of
       (T.unpack -> '<':_) -> Nothing -- one of the <built-in>, <entire-module>, etc.
-      (txt2span' -> Just (fsrc, (al:ac:bl:bc:_))) ->
-        Just $ mkRealSrcSpan (mkRealSrcLoc fsrc al ac) (mkRealSrcLoc fsrc bl bc) -- fsLit $ T.unpack $ costCentreModule
+      (txt2span_multi -> Just (fsrc, (al:ac:bl:bc:_))) ->
+        Just $ mkRealSrcSpan (mkRealSrcLoc fsrc al ac) (mkRealSrcLoc fsrc bl (bc + 1)) -- fsLit $ T.unpack $ costCentreModule -- +1 because somehow the HIE loc and the prof file loc are different with their inclusive/exclusive
+      (txt2span_single -> Just (fsrc, (l:ac:bc:_))) ->
+        Just $ mkRealSrcSpan (mkRealSrcLoc fsrc l ac) (mkRealSrcLoc fsrc l (bc + 1)) -- fsLit $ T.unpack $ costCentreModule
       _ -> Nothing
     -- txt2span' :: T.Text -> Maybe (FastString, [Int])
-    txt2span' = fmap (fsLit *** map read) . uncons . map T.unpack . getAllTextSubmatches . (=~ ("^(.*?):\\((\\d+),(\\d+)\\)-\\((\\d+),(\\d+)\\)$" :: T.Text))
+    txt2span_multi = fmap (fsLit *** mapMaybe readMaybe) . ((uncons . snd) =<<) . uncons . map T.unpack . getAllTextSubmatches . (=~ ("^(.*):\\(([0-9]+),([0-9]+)\\)\\-\\(([0-9]+),([0-9]+)\\)$" :: T.Text)) -- safe drop 1
+    txt2span_single = fmap (fsLit *** mapMaybe readMaybe) . ((uncons . snd) =<<) . uncons . map T.unpack . getAllTextSubmatches . (=~ ("^(.*):([0-9]+):([0-9]+)\\-([0-9]+)$" :: T.Text)) -- . uncurry (trace . show) . dupe
 
 fNS :: [FastString]
 fNS = ["HsLam", "AbsBinds"] -- I really hope the trees look the same for these two always every time: single level of first arg as 
@@ -190,46 +192,58 @@ mk_pt_store dflags = ((ps_app_groups . ag_span_map) %~ (\(SegFlat s) -> SegTree 
           --         else next_store
           --   in ( this_names <> next_names, next_store' )
 
-pt_search :: DynFlags -> PtStore a -> Segs (Maybe Prof.CostCentre) -> Bool -> Identifier -> (Maybe Gr.Node, Gr (NodeKey a) ())
-pt_search dflags (PtStore {..}) cs_segs =
+pt_search :: DynFlags -> PtStore a -> Segs Prof.CostCentre -> Either FnKey Identifier -> (Maybe Gr.Node, Gr (NodeKey a) ())
+pt_search dflags (PtStore {..}) cs_segs = 
   let state_step :: [NodeKey a] -> (M.Map (NodeKey a) Gr.Node, Gr.Node) -> (M.Map (NodeKey a) Gr.Node, Gr.Node)
+      state_step [] t = t
       state_step l (a, s) = ((M.union a . M.fromList) &&& snd . last) $ zip l [s..] -- (((a,) . M.fromList) &&& snd . last) $ zip l [s..]
       -- nodes :: M.Map (NodeKey a) Gr.Node
-      nodes = (`evalState` 0) $
+      nodes = -- (uncurry $ uncurry $ uncurry $ NodeStore . flip const)
+        (`evalState` 0) $
           return mempty
-          & mapState (state_step (map NKApp $ M.elems (_ps_app_groups ^. ag_ident_map)))
+          & mapState (state_step (map NKApp $ concat $ M.elems (_ps_app_groups ^. ag_ident_map)))
           & mapState (state_step (map NKBind $ M.elems (_ps_binds)))
           & mapState (state_step (map NKFn $ M.elems (_ps_fns)))
         
       g0 = Gr.mkGraph (map swap $ M.toList nodes) mempty
+      r0 = (Nothing, g0)
       
       -- merge_decomps :: [(Maybe Gr.Node, Gr (NodeKey a) b)] -> ([Maybe Gr.Node], Gr (NodeKey a) b)
-      merge_decomps = foldr1 (uncurry bimap . ((<>) *** flip (foldr Gr.insEdge) . Gr.labEdges)) . fmap (first pure) . toNonEmpty (Nothing, g0) -- egregiously wasteful; think of a better way
+      merge_decomps = foldr1 (uncurry bimap . ((<>) *** flip (foldr Gr.insEdge) . Gr.labEdges)) . fmap (first pure) . toNonEmpty r0 -- egregiously wasteful; think of a better way
       
       -- pt_search' :: Bool -> Identifier -> (Maybe Gr.Node, Gr (NodeKey a) ())
-      pt_search' visited is_fn ident =
-        let ((next_nodes, next_gr), this_node)
-              | is_fn
-              , Just ag <- (_ps_app_groups ^. ag_ident_map) M.!? ident
-              , not $ ag `S.member` visited
-              -- , Nothing <- (nodes !? (NKApp ag)) >>= (flip match gr) -- crap, can't build the graph from the bottom up because I need to anti-cycle, so children will have to do the matching
-              = (merge_decomps $ map (pt_search' (S.insert ag visited) False) ag, nodes M.!? NKFn (FnNamed ident))
-              
-              | Just fnkey <- _ps_fns M.!? ident
-              , m_this_node <- nodes M.!? (NKFn fnkey)
-              = case fnkey of
-                FnNamed fn_ident -> (first pure $ pt_search' visited True fn_ident, m_this_node)
-                FnLam fn_span ->
-                  let next_nodes' = concat $ segfind (_ps_app_groups ^. ag_span_map) fn_span
-                  in (merge_decomps $ map (pt_search' visited False) next_nodes', m_this_node)
-                  
-              | Just bg@(BindGroup _sp pts) <- _ps_binds M.!? ident
-              , m_this_node <- nodes M.!? (NKBind bg)
-              = (merge_decomps $ map (pt_search' visited False) (concat $ M.elems $ pts ^. ps_app_groups ^. ag_ident_map), m_this_node) -- TODO figure out what bindees are actually pointing to: for now, only bind directly to the shallow app-names          
-              
-              | otherwise = ((mempty, g0), Nothing)
-          in snd (dflags, (this_node, foldr (\node -> fromMaybe id $ Gr.insEdge <$> ((, node, ()) <$> this_node)) next_gr (catMaybes next_nodes)))
-      
+      pt_search' visited m_ident = 
+        let (((next_nodes, next_gr), this_node), within_cs) = case m_ident of
+              Left fk -> (
+                  if | m_this_node <- nodes M.!? (NKFn fk)
+                     -> case fk of
+                       FnNamed _fn_span fn_ident -> (first pure $ pt_search' visited (Right fn_ident), m_this_node)
+                       FnLam fn_span ->
+                         let next_nodes' = concatMap s_payload $ segfind (_ps_app_groups ^. ag_span_map) fn_span
+                         in (merge_decomps $ map (pt_search' visited . Right) next_nodes', m_this_node)
+                  , not $ null $ segfind cs_segs $ fnkey_span fk
+                )
+              Right ident -> (
+                  if | Just ags' <- (_ps_app_groups ^. ag_ident_map) M.!? ident
+                     , ags <- S.fromList ags' S.\\ visited
+                     -- , Nothing <- (nodes !? (NKApp ag)) >>= (flip match gr) -- crap, can't build the graph from the bottom up because I need to anti-cycle, so children will have to do the matching
+                     -> flip const dflags $ (
+                         merge_decomps $ map (pt_search' (S.union visited ags) . Right) $ concatMap s_payload $ S.toList ags
+                         , nodes M.!? NKFn (FnNamed undefined ident) -- don't worry about the undefined: the Map doesn't pay attention to it for the lookup
+                       )
+                         
+                     | Just bg@(BindGroup _sp pts) <- uncurry trace $ first (((ppr_safe dflags ident)++) . ppr_safe dflags) $ dupe $ _ps_binds M.!? ident -- uncurry trace $ first (((ppr_safe dflags ident)++) . ppr_safe dflags) $ dupe $ 
+                     , m_this_node <- nodes M.!? (NKBind bg)
+                     , ags <- pts ^. ps_app_groups ^. ag_ident_map
+                     -> (merge_decomps $ map (pt_search' (S.union visited (S.fromList $ concat $ M.elems ags)) . Right) (concatMap s_payload $ concat $ M.elems ags), m_this_node) -- TODO figure out what bindees are actually pointing to: for now, only bind directly to the shallow app-names          
+                     
+                     | otherwise -> ((mempty, g0), Nothing)
+                  , ((not . null . segfind cs_segs) <$> (ident_span ident)) `elem` [Nothing, Just True]
+                )
+        in if within_cs
+          then (this_node, foldr (\node -> fromMaybe id $ Gr.insEdge <$> ((, node, ()) <$> this_node)) next_gr (catMaybes next_nodes))
+          else r0
+        
       -- pt_search False ident
       --   | 
       --   , Just this_node <- nodes M.!? fnkey
@@ -254,43 +268,80 @@ main :: IO ()
 main = do
   dflags <- dynFlagsForPrinting
   nc <- makeNc
-  (targ:fprof:dhies) <- getArgs
+  (s_targ:fprof:dhies) <- getArgs
   fhies <- fmap concat $ mapM getHieFilesIn $ dhies
   (hies, _) <- foldrM (\f (l, nc') -> (first ((:l) . hie_file_result)) <$> readHieFile nc' f) ([], nc) fhies
-  
+  -- print fhies
   -- putStrLn $ unlines $ map (ppr_ast dflags) $ M.elems $ getAsts $ hie_asts $ head hies
   
   mprof <- Prof.decode' <$> TIO.readFile fprof
   case mprof of
     Left s -> error s
-    Right (Prof.costCentres -> Just cs_tree) ->
-      let (Just loc_cs_tree) = Tr.foldTree (curry $ fmap (uncurry Tr.Node) . bisequence . (bisequence . (pure &&& cs_span) *** pure . catMaybes)) cs_tree -- (SrcSpan, Profile)
-          pts = map (mk_pt_store dflags) (concatMap (M.elems . getAsts . hie_asts) hies)
-          ident_str_lookup = M.fromList . map ((\case { Left mn -> moduleNameString mn; Right n -> occNameString $ nameOccName n }) &&& id)
+    Right (Prof.costCentres -> Just cs_tree) -> -- curry $ fmap (uncurry Tr.Node) . bisequence .
+      let loc_cs_forest = Tr.foldTree (
+              curry $
+              uncurry (flip fromMaybe)
+              . ((uncurry (liftA2 (fmap (\x -> [x]) . Tr.Node)) . second pure) &&& snd)
+              . (bisequence . (pure &&& cs_span) *** concat)
+            ) cs_tree -- (SrcSpan, Profile)
+          pts = map (mk_pt_store dflags) (concatMap (uncurry traceShow . first length . dupe . M.elems . getAsts . hie_asts) hies)
           -- note: also able to map directly to Binds and FnKeys as well, since the PtStore map type right-hands can be coalesced to NodeKeys
-          bound = M.unionsWith (<>) (
-              map (M.map pure . ident_str_lookup . map fst . M.toList . (^. ps_binds)) pts
-              ++ map (
+          bound'' :: STree.STree [STInterval.Interval (Locd ())] (Locd ())
+          bound'' = STree.fromList $ concatMap (map (seg2intervalish . (fnkey_span &&& const ())) . M.elems . (^. ps_fns)) pts
+          bound' = concatMap (map (seg2intervalish . (fnkey_span &&& id)) . M.elems . (^. ps_fns)) pts
+          bound = STree.fromList bound'
+          
+          ident_str_lookup = M.fromList . mapMaybe (\case
+              FnLam _ -> Nothing
+              FnNamed s i -> Just $ (
+                case i of 
+                  Left mn -> (moduleNameString mn, "")
+                  Right n -> (fromMaybe "" $ moduleNameString . moduleName <$> nameModule_maybe n, occNameString $ nameOccName n)
+                , s)
+            )
+          bound''' :: M.Map (String, String) [Span]
+          bound''' = M.unionsWith (<>) (
+              -- map (M.map pure . ident_str_lookup . M.keys . (^. ps_binds)) pts -- i don't actually think that binds are referenced in the cost center stack trace
+              -- ++
+              map (
                   M.map pure
-                  . uncurry (M.unionWith (<>))
-                  . (
-                      ident_str_lookup
-                      *** ident_str_lookup . mapMaybe (\case { FnNamed i -> Just i; FnLam _ -> Nothing })
-                    )
-                  . unzip
-                  . M.toList
+                  . ident_str_lookup
+                  . M.elems
                   . (^. ps_fns)
                 ) pts
             )
+          i_targ = read s_targ :: Int
+          targ_fold css@(cs, _) l = 
+            let next = if Prof.costCentreNo cs == i_targ then Just css else Nothing
+                l' = mconcat $ map (First . bisequence . second pure) l
+            in case getFirst l' of -- pass up entire subtree if we haven't found the module yet, otherwise constrain to single branch
+              Just (cs_matched, t) -> (Just cs_matched, Tr.Node css [t])
+              Nothing -> (next, Tr.Node css (map snd l))
+          (targ, targ_t) = unzip $ mapMaybe (bisequence . second pure . Tr.foldTree targ_fold) loc_cs_forest
+          -- targ_segs' = SegTree $ STree.fromList $ concatMap (Tr.foldTree ((.concat) . (:) . seg2intervalish . second (const ()) . swap)) targ_t
+          targ_segs = SegTree $ STree.fromList $ concatMap (Tr.foldTree ((.concat) . (:) . seg2intervalish . swap)) targ_t
+          fks = ivl_payloads $ concatMap (STree.superintervalQuery bound . span2interval . snd) targ
           grs = [
-              pt_search dflags pt undefined False ident
-              | idents <- maybeToList $ (bound M.!? targ)
-              , ident <- idents
+              pt_search dflags pt targ_segs (Left fk)
+              | fk <- fks
               , pt <- pts
             ]
-      in do
-        putStrLn $ unlines $ const (map (gr_prettify (ppr_safe dflags) id . snd) grs) $ map (\case 
+      in flip const (targ, bound, dflags, loc_cs_forest, pts, grs) $ do -- $ trace (show $ length loc_cs_forest)
+        pure ()
+        -- putStrLn $ ppr_safe dflags $ M.elems . (^. ps_fns) <$> pts
+        putStrLn $ show $ [(b, sp, b == sp) | (_, sp) <- targ, b <- (bound''' M.! ("Main", "has_node_constr"))]
+        print $ concatMap (\(_, s) -> ivl_locs $ STree.superintervalQuery bound $ span2interval s) targ
+        -- print bound''
+        -- putStrLn $ ppr_safe dflags bound'
+        print (length fks, map ((length . (_ag_ident_map . _ps_app_groups) &&& length . _ps_fns &&& length . _ps_binds)) pts)
+        putStrLn $ Tr.drawForest $ fmap (show . snd) <$> targ_t
+        -- ppr_ dflags $ map (S.fromList . M.elems . _ag_ident_map . _ps_app_groups) pts
+        -- -- putStrLn $ ppr_safe dflags bound -- <$> (bound M.!? targ)
+        putStrLn $ unlines $ map (\(n, gr) ->
+            unlines $ map (uncurry ((++) . (++"->")) . both (fromMaybe "" . fmap (ppr_nk dflags) . Gr.lab gr)) $ S.toList $ S.fromList $ Gr.edges gr
+          ) grs
+        {- $ map (\case -- gr_prettify (ppr_safe dflags) id
             (Just node, gr) -> gr_prettify (ppr_safe dflags) id gr
             (Nothing, _) -> mempty
-          ) grs
+          ) grs -}
         
